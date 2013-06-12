fae98707f83873b604d7023a23d75adf1b94ad10
Jakub Wawrzyniak <jakub@itpeople.pl>
2013-06-12 22:23:14 +0200

    messages

--- protected/controllers/MessagesController.php
+++ protected/controllers/MessagesController.php
@@ -28,12 +28,8 @@ class MessagesController extends Controller
 	{
 		return array(
 			array('allow', // allow authenticated user to perform 'create' and 'update' actions
-				'actions'=>array('index','view','create','update'),
+				'actions'=>array('index','view','downloadAttachment'),
 				'users'=>array('@'),
-			),
-			array('allow', // allow admin user to perform 'admin' and 'delete' actions
-				'actions'=>array('admin','delete'),
-				'users'=>array('admin'),
 			),
 			array('deny',  // deny all users
 				'users'=>array('*'),
@@ -52,6 +48,159 @@ class MessagesController extends Controller
 		$this->render('index', array(
 				'mailbox' => $mailbox,
 			));
+	}
+	
+	public function actionView($msgno=null)
+	{
+		if($msgno !== null)
+		{
+			$group = Groups::model()->findByPk(Yii::app()->user->gid);
+		
+			$mailbox['name'] = $group->name;
+			$mailbox['pass'] = $group->password;
+			
+			$this->render('view', array(
+					'msgno' => $msgno,
+					'mailbox' => $mailbox,
+				));
+		}
+		else
+		{
+			$this->redirect('index');
+		}
+	}
+	
+	public function actionDownloadAttachment($msgno, $partno)
+	{
+		$group = Groups::model()->findByPk(Yii::app()->user->gid);
+		if($mbox = @imap_open("{".Yii::app()->params['hostURL'].":".Yii::app()->params['hostPort']."/pop3/novalidate-cert}INBOX", $group->name.'@'.Yii::app()->params['hostURL'], $group->password, OP_SILENT))
+		{
+			$structure = imap_fetchstructure($mbox, $msgno);
+
+			$encoding = $structure->parts[$partno]->encoding;
+			//extract file name from headers
+			$fileName = imap_mime_header_decode(strtolower($structure->parts[$partno]->dparameters[0]->value))[0]->text;
+			//extract attachment from email body
+			$fileSource = base64_decode(imap_fetchbody($mbox, $msgno, $partno+1));
+			
+			//get extension
+			$ext = substr($fileName, strrpos($fileName, '.') + 1);
+			//die($ext);
+	
+			//get mime file type
+			switch ($ext) {
+			case "asf":
+				$type = "video/x-ms-asf";
+				break;
+			case "avi":
+				$type = "video/avi";
+				break;
+			case "flv":
+				$type = "video/x-flv";
+				break;
+			case "fla":
+				$type = "application/octet-stream";
+				break;
+			case "swf":
+				$type = "application/x-shockwave-flash";
+				break;		
+			case "doc":
+				$type = "application/msword";
+				break;
+			case "docx":
+				$type = "application/msword";
+				break;
+			case "zip":
+				$type = "application/zip";
+				break;
+			case "xls":
+				$type = "application/vnd.ms-excel";
+				break;
+			case "gif":
+				$type = "image/gif";
+				break;
+			case "jpg" || "jpeg":
+				$type = "image/jpg";
+				break;
+			case "png":
+				$type = "image/png";
+				break;		
+			case "wav":
+				$type = "audio/wav";
+				break;
+			case "mp3":
+				$type = "audio/mpeg3";
+				break;
+			case "mpg" || "mpeg":
+				$type = "video/mpeg";
+				break;
+			case "rtf":
+				$type = "application/rtf";
+				break;
+			case "htm" || "html":
+				$type = "text/html";
+				break;
+			case "xml":
+				$type = "text/xml";
+				break;	
+			case "xsl":
+				$type = "text/xsl";
+				break;
+			case "css":
+				$type = "text/css";
+				break;
+			case "php":
+				$type = "text/php";
+				break;
+			case "txt":
+				$type = "text/txt";
+				break;
+			case "asp":
+				$type = "text/asp";
+				break;
+			case "pdf":
+				$type = "application/pdf";
+				break;
+			case "psd":
+				$type = "application/octet-stream";
+				break;
+			default:
+				$type = "application/octet-stream";
+			}
+			@imap_close($mbox);
+			
+			header('Content-Description: File Transfer');
+			header('Content-Type: '.$type);
+			header('Content-Disposition: attachment; filename='.basename($fileName));
+			header('Content-Transfer-Encoding: binary');
+			header('Expires: 0');
+			header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+			header('Pragma: public');
+			//header('Content-Length: ' . filesize($fileSource));
+			ob_clean();
+			flush();
+			echo $fileSource;
+			
+			//$request = new CHttpRequest();
+			//if(file_put_contents(Yii::app()->basePath.'/../attachments/'.$fileName, $fileSource))
+			//{
+			//	header('Content-Description: File Transfer');
+			//	header('Content-Type: '.$type);
+			//	header('Content-Disposition: attachment; filename='.basename($fileName));
+			//	header('Content-Transfer-Encoding: binary');
+			//	header('Expires: 0');
+			//	header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+			//	header('Pragma: public');
+			//	header('Content-Length: ' . filesize(Yii::app()->basePath.'/../attachments/'.$fileName));
+			//	ob_clean();
+			//	flush();
+			//	readfile(Yii::app()->basePath.'/../attachments/'.$fileName);
+			//}
+		}
+		else
+		{
+			var_dump(imap_errors());
+		}
 	}
 
 }
---
+++ protected/views/messages/view.php
@@ -0,0 +1,140 @@
+<?php
+/* @var $this MessagesController */
+
+$this->breadcrumbs=array(
+	'Messages',
+);
+
+$this->menu=array(
+	array('label'=>'WIADOMOŚCI'),
+	array('label'=>'Odebrane', 'url'=>array('messages/index'), 'active'=>true, 'icon'=>'envelope'),	
+	);
+
+//imap functions
+function flattenParts($messageParts, $flattenedParts = array(), $prefix = '', $index = 1, $fullPrefix = true) {
+
+	foreach($messageParts as $part) {
+		$flattenedParts[$prefix.$index] = $part;
+		if(isset($part->parts)) {
+			if($part->type == 2) {
+				$flattenedParts = flattenParts($part->parts, $flattenedParts, $prefix.$index.'.', 0, false);
+			}
+			elseif($fullPrefix) {
+				$flattenedParts = flattenParts($part->parts, $flattenedParts, $prefix.$index.'.');
+			}
+			else {
+				$flattenedParts = flattenParts($part->parts, $flattenedParts, $prefix);
+			}
+			unset($flattenedParts[$prefix.$index]->parts);
+		}
+		$index++;
+	}
+
+	return $flattenedParts;
+			
+}
+
+function getPart($connection, $messageNumber, $partNumber, $encoding) {
+	
+	$data = imap_fetchbody($connection, $messageNumber, $partNumber);
+	switch($encoding) {
+		case 0: return $data; // 7BIT
+		case 1: return $data; // 8BIT
+		case 2: return $data; // BINARY
+		case 3: return base64_decode($data); // BASE64
+		case 4: return quoted_printable_decode($data); // QUOTED_PRINTABLE
+		case 5: return $data; // OTHER
+	}
+	
+	
+}
+
+function getFilenameFromPart($part) {
+
+	$filename = '';
+	
+	if($part->ifdparameters) {
+		foreach($part->dparameters as $object) {
+			if(strtolower($object->attribute) == 'filename') {
+				$filename = $object->value;
+			}
+		}
+	}
+
+	if(!$filename && $part->ifparameters) {
+		foreach($part->parameters as $object) {
+			if(strtolower($object->attribute) == 'name') {
+				$filename = $object->value;
+			}
+		}
+	}
+	
+	return $filename;
+	
+}
+
+?>
+<h3>Wiadomość #<?php echo $msgno; ?> - Szczegóły</h3>
+<?php
+
+if($mbox = @imap_open("{".Yii::app()->params['hostURL'].":".Yii::app()->params['hostPort']."/pop3/novalidate-cert}INBOX", $mailbox['name'].'@'.Yii::app()->params['hostURL'], $mailbox['pass'], OP_SILENT))
+{
+	$structure = imap_fetchstructure($mbox, $msgno);
+        
+        $flattenedParts = flattenParts($structure->parts);
+        $attachments = array();
+        
+        foreach($flattenedParts as $partNumber => $part) {
+
+            switch($part->type) {
+		
+		case 0:
+			$message = getPart($mbox, $msgno, $partNumber, $part->encoding);
+		break;
+	
+		case 1:
+			// multi-part headers, can ignore
+	
+		break;
+		case 2:
+			// attached message headers, can ignore
+		break;
+	
+		case 3: // application
+		case 4: // audio
+		case 5: // image
+		case 6: // video
+		case 7: // other
+                        $attachments[] = array(
+                            'partno' => $partNumber-1,
+                            'fileName' => mb_convert_encoding(imap_mime_header_decode(getFilenameFromPart($part))[0]->text, 'utf-8', 'iso-8859-2'),
+                        );
+		break;
+	
+            }
+	
+        }
+        
+	$overview = imap_fetch_overview($mbox, $msgno, 0);
+        
+	echo '<div class="well"><strong>Od:</strong> '.$overview[0]->from.'<br><strong>Temat:</strong> '.imap_utf8($overview[0]->subject).'<br><strong>Otrzymano:</strong> '.date('Y.m.d', strtotime($overview[0]->date)).'<hr><p>'.mb_convert_encoding($message, 'utf-8', mb_detect_encoding($message, array('iso-8859-2', 'utf-8', 'iso-8859-1'))).'</p></div>';
+        
+        
+        if(sizeof($attachments)>0)
+        {
+            echo '<h4>Załączniki:</h4>';
+            foreach($attachments as $idx=>$attachment)
+            {
+                echo ($idx+1).'. <a href="'.Yii::app()->createUrl('messages/downloadAttachment', array('msgno'=>$msgno, 'partno'=>$attachment['partno'])).'">'.$attachment['fileName'].'</a><br>';
+            }    
+        }
+        
+        
+	@imap_close($mbox);
+}
+else
+{
+	var_dump(imap_errors());
+}
+
+?>
